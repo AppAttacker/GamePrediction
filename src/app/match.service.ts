c33c25c11cde4ad3b3232b179d934ec4e253ea57
Perumal <470046@cognizant.com>
2019-04-12 11:26:10 +0530

    mock service access

--- src/app/match.service.ts
+++ src/app/match.service.ts
@@ -10,7 +10,7 @@ export class MatchService {
 
   constructor(private http: HttpClient) { }
 
-  restURL: string = 'http://localhost:8080/game/matchschedule';
+  restURL: string = 'http://10.252.52.159:8080/game/matchschedule';
   
   getMatchScheduleList(): Observable<IMatchDetails[]>{
     return this.http.get<IMatchDetails[]>(this.restURL);
