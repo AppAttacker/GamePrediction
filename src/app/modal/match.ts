63f31894535cd6d3dd42c025a394be7328d7b24b
Perumal <470046@cognizant.com>
2019-05-09 12:45:09 +0530

    patti and tingaring works done..

--- src/app/modal/match.ts
+++ src/app/modal/match.ts
@@ -1,10 +1,9 @@
 import { Country } from './country';
-import { Time } from '@angular/common';
-
 
 export class Match {
     matchid: string;
     startTime: string;
     fromTeam: Country;
     toTeam: Country;
+    venue: string;
 }
